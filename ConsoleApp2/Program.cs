56ed3cf022f8509f56ac348fa1158995b7196ae8
ricar <ricar@DESKTOP-K2KF8A1>
2023-01-29 11:41:24 -0300

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+
+
+            Console.WriteLine("Yeah Bitch\n");
+            Console.WriteLine("Como é seu nome? \n");
+            String nome = Console.ReadLine();
+            
+
+            string NOME = nome.ToUpper();
+            string nome2 = NOME.ToLower();
+            string codeName = NOME.Insert(0, "MR. ");
+            Console.WriteLine("Seu codinome é " + codeName + " KING");
+            Console.WriteLine("Seu nome tem " + NOME.Length + " Caracteres.");
+            Console.WriteLine("\nMe diga 2 estilos de cerveja: \n");
+
+
+            String beertype1 = Console.ReadLine();
+            String beertype2 = Console.ReadLine();
+         
+
+            Console.WriteLine("\nSeu nome é " + codeName + " KING, e você gosta de " + beertype1 + " e " + beertype2 + ".\n\n");
+            Console.WriteLine("\"Beer is proof that God loves us and wants us to be happy.\"");
+
+
+            Console.ReadKey();
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
